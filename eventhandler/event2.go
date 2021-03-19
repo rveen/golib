4eb0a3e218f79aeebbd7f048ce04720616f04f65
rveen <rolf.veen@gmail.com>
2021-03-19 10:45:13 +0100

    ...

--- eventhandler/event2.go
+++ eventhandler/event2.go
@@ -8,23 +8,24 @@ package eventhandler
 
 import "github.com/rveen/ogdl"
 
-// EventHandler receives events and produces a tree.
-type EventHandler struct {
-	current int      // Current level
-	max     int      // Max level
-	levels  []int    // Level of each item
-	items   []string // Items
+// EventHandler2 receives events and produces a tree.
+// TODO benchmark agains the pure string version. Keep this one if difference is small
+type EventHandler2 struct {
+	current int           // Current level
+	max     int           // Max level
+	levels  []int         // Level of each item
+	items   []interface{} // Items
 }
 
-func New() *EventHandler {
-	return &EventHandler{}
+func New2() *EventHandler2 {
+	return &EventHandler2{}
 }
 
-func (e *EventHandler) Len() int {
+func (e *EventHandler2) Len() int {
 	return len(e.levels)
 }
 
-func (e *EventHandler) Item(i int) (string, int) {
+func (e *EventHandler2) Item(i int) (interface{}, int) {
 	if i < 0 || i >= len(e.levels) {
 		return "", -1
 	}
@@ -32,13 +33,13 @@ func (e *EventHandler) Item(i int) (string, int) {
 }
 
 // Add creates a string node at the current level.
-func (e *EventHandler) Add(s string) {
+func (e *EventHandler2) Add(s interface{}) {
 	e.items = append(e.items, s)
 	e.levels = append(e.levels, e.current)
 }
 
 // AddAt creates a string node at the specified level.
-func (e *EventHandler) AddAt(s string, lv int) {
+func (e *EventHandler2) AddAt(s interface{}, lv int) {
 	e.items = append(e.items, s)
 	e.levels = append(e.levels, lv)
 	if e.max < lv {
@@ -47,18 +48,18 @@ func (e *EventHandler) AddAt(s string, lv int) {
 }
 
 // Delete removes the last node added
-func (e *EventHandler) Delete() {
+func (e *EventHandler2) Delete() {
 	e.items = e.items[0 : len(e.items)-1]
 	e.levels = e.levels[0 : len(e.levels)-1]
 }
 
 // Level returns the current level
-func (e *EventHandler) Level() int {
+func (e *EventHandler2) Level() int {
 	return e.current
 }
 
 // SetLevel sets the current level
-func (e *EventHandler) SetLevel(l int) {
+func (e *EventHandler2) SetLevel(l int) {
 	e.current = l
 	if e.max < l {
 		e.max = l
@@ -66,7 +67,7 @@ func (e *EventHandler) SetLevel(l int) {
 }
 
 // Inc increments the current level by 1.
-func (e *EventHandler) Inc() {
+func (e *EventHandler2) Inc() {
 	e.current++
 	if e.max < e.current {
 		e.max = e.current
@@ -74,7 +75,7 @@ func (e *EventHandler) Inc() {
 }
 
 // Dec decrements the current level by 1.
-func (e *EventHandler) Dec() {
+func (e *EventHandler2) Dec() {
 	if e.current > 0 {
 		e.current--
 	}
@@ -83,7 +84,7 @@ func (e *EventHandler) Dec() {
 // Tree returns the Graph object built from
 // the events sent to this event handler.
 //
-func (e *EventHandler) Graph() *ogdl.Graph {
+func (e *EventHandler2) Graph() *ogdl.Graph {
 
 	g := make([]*ogdl.Graph, e.max+2)
 	g[0] = ogdl.New(nil)
